a1f085cabaede0f79e025e3f07caeae8f957b328
xiaotongwong <wongxiaotong1@gmail.com>
2023-03-17 15:33:29 +0800

    develop-cam-group-resource

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,11 +1,21 @@
 terraform {
   required_providers {
     st-tencentcloud = {
-      source = "myklst/st-tencentcloud"
+      source = "example.local/myklst/st-tencentcloud"
     }
   }
 }
 
 provider "st-tencentcloud" {
   region = "ap-hongkong"
+}
+
+resource "st-tencentcloud_cam_group_membership" "test" {
+  group_id = 271589
+  user_id = 15565624
+}
+
+resource "st-tencentcloud_cam_group_membership" "test2" {
+  group_id = 271589
+  user_id = 15565625
 }
